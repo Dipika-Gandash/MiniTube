ae0fdce430b095cb7e1c809eadb0e8254991d4aa
Dipika Gandash <dipikadevv1445@gmail.com>
2025-12-24 23:13:31 +0530

    add MiniTube header with grid layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import hamburgerIcon from "../assets/hamburger_icon.png";
+import searchIcon from "../assets/search_icon.png";
+import userIcon from "../assets/user_icon.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="grid grid-cols-[1fr_3fr_1fr] items-center shadow-md p-4">
+      <div className="flex gap-3 items-center">
+        <img
+          src={hamburgerIcon}
+          alt="menu"
+          className="w-8 h-8 cursor-pointer"
+        />
+        <h1 className="text-3xl font-bold text-red-500 cursor-pointer">
+          MiniTube
+        </h1>
+      </div>
 
-export default Header
+      <form
+        className="flex justify-center"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <div className="flex w-full max-w-[600px]">
+          <input
+            type="text"
+            placeholder="Search"
+            className="flex-1 py-3 pl-5 border border-gray-500 rounded-l-full outline-none focus:ring-1 focus:ring-red-700"
+          />
+          <button className="border border-gray-500 rounded-r-full px-4 bg-gray-200">
+            <img src={searchIcon} alt="search" className="w-6 h-6" />
+          </button>
+        </div>
+      </form>
+
+      <div className="flex justify-end mr-5">
+        <img src={userIcon} alt="user" className="w-12 h-12 cursor-pointer" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
